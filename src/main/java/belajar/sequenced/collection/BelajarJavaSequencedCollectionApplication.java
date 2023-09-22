3506d32c87416cdb3c75d5b8a4c3ceefbc2326c9
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-09-22 18:54:14 +0700

    belajar sequenced collection

---
+++ src/main/java/belajar/sequenced/collection/BelajarJavaSequencedCollectionApplication.java
@@ -0,0 +1,13 @@
+package belajar.sequenced.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaSequencedCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaSequencedCollectionApplication.class, args);
+	}
+
+}
